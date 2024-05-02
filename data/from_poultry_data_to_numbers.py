91e59a1f4e8817e12fa5d9c069165203a5a1804a
thomas mcandrew <mcandrew@lehigh.edu>
2024-05-02 13:56:48 -0400

    Updates

--- data/from_poultry_data_to_numbers.py
+++ data/from_poultry_data_to_numbers.py
@@ -8,34 +8,25 @@ from datetime import datetime, timedelta
 
 if __name__ == "__main__":
 
-    d = pd.read_csv("./A Table by Confirmation Date.csv",sep="\t",encoding="utf-16")
-    d = d.replace(np.nan,0)
+    d = pd.read_csv("./commercial-backyard-flocks.csv")
+    d["day"] = [ datetime.strptime(x,"%m-%d-%Y").strftime("%Y-%m-%d")  for x in d["Outbreak Date"].values]
 
-    control_area_cols = [x for x in d.columns if "Control Area" in x]
-    d = d.replace(',','', regex=True)
-
-    d = d.iloc[1:]
-
-    for col in control_area_cols:
-        d[col] = d[col].astype(float)
-    
-    d["birds_affected"] = d.loc[:,control_area_cols].sum(1)
-    d["date"]           = [ datetime.strptime(x,"%d-%b-%y").strftime("%Y-%m-%d") for x in d.iloc[:,0]]
-
-    d = d[ ["date","birds_affected"] ]
-
-    from_day_to_week = {"date": d.date.unique()}
-    weeks = [ Week.fromdate(datetime.strptime(date,"%Y-%m-%d")).cdcformat()  for date in from_day_to_week["date"]  ]
+    from_day_to_week = {"day": d.day.unique()}
+    weeks = [ Week.fromdate(datetime.strptime(day,"%Y-%m-%d")).cdcformat()  for day in from_day_to_week["day"]  ]
     from_day_to_week["week"] = weeks
-    from_day_to_week         = pd.DataFrame(from_day_to_week)
+    from_day_to_week = pd.DataFrame(from_day_to_week)
+    
+    def count(x):
+        num_birds = x["Flock Size"].sum()
+        return pd.Series({"num_birds": num_birds})
+    groups = d.groupby(["day"]).apply( count ).reset_index()
+    groups = groups.merge( from_day_to_week, on = ["day"] )
 
-    d = d.merge( from_day_to_week, on = ["date"] )
- 
     #--aggregate to epidemic week level
     def addup(x):
-        return pd.Series({ "birds_affected": x.birds_affected.sum() })
-    week_level = d.groupby(["week"]).apply(addup).reset_index()
+        return pd.Series({ "num_birds":x.num_birds.sum()})
+    week_level = groups.groupby(["week"]).apply(addup).reset_index()
     week_level["elapsed_weeks"] = np.arange(len(week_level))
 
-    week_level.to_csv("./weekly_incident_poultry_aphis.csv",index=False)
-    week_level.to_csv("./arxiv/weekly_incident_poultry_aphis__{:s}.csv".format(datetime.today().strftime("%Y-%m-%d")),index=False)
+    week_level.to_csv("./weekly_incident_wild_birds_aphis.csv",index=False)
+    week_level.to_csv("./arxiv/weekly_incident_wild_birds_aphis__{:s}.csv".format(datetime.today().strftime("%Y-%m-%d")),index=False)
